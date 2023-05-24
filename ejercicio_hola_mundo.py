5e97d8b061259ad2143b394efeea801319c4e1f7
leonidasvelasquez <e.velasquez@profesor.duoc.cl>
2023-05-24 23:44:22 +0000

    actualizacion de los comandos

--- ejercicio_hola_mundo.py
+++ ejercicio_hola_mundo.py
@@ -1,6 +1,6 @@
 print("HOlA GITHUB")
 
-# comandos
-# git add .
-# git commit -m "primer commit"
-# git push origin main
+# orden de los comandos
+# 1. git add .
+# 2. git commit -m "primer commit"
+# 3. git push origin main
